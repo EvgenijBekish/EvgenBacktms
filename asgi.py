a1c1e8dcbe14aca6392ae915a1024ed2554ca583
EvgenijBekish <yauhenibekish@gmail.com>
2021-08-20 21:48:15 +0300

    pyCharm

--- asgi.py
+++ asgi.py
@@ -6,6 +6,8 @@ from fastapi import FastAPI
 from fastapi import Query
 from starlette.requests import Request
 from starlette.responses import Response
+import psycopg2
+
 
 from lessons import task_3_1
 
@@ -29,6 +31,22 @@ def get_user(request: Request):
     return request.cookies.get("user")
 
 
+def get_sum(user: str) -> int:
+    sql = f"""
+    SELECT n FROM numbers
+    WHERE name =  '{name}'
+    ;
+    """
+    r = execute_sql(sql)
+
+def save_number(user: str, number: int):
+    if user_exist(user):
+        update_number(user, number)
+    else:
+        insert_new_user(user, number)
+
+
+
 @app.post("/task/4")
 def handler(
         request: Request,
@@ -37,10 +55,16 @@ def handler(
 ):
     user = get_user(request) or gen_random_name()
     response.set_cookie("user", user)
+    # 
+    # if data == "stop":
+    #     return sum(numbers[user])
+    # else:
+    #     assert data.isdigit()
+    #     numbers[user].append(int(data))
+    #     return numbers[user]
 
     if data == "stop":
-        return sum(numbers[user])
+        return get_sum(user)
     else:
-        assert data.isdigit()
-        numbers[user].append(int(data))
-        return numbers[user]
+        save_number(user, int(data))
+        return data
