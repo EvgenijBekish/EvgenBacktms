38a4ab4631067367139de41707b8aac826047211
EvgenijBekish <yauhenibekish@gmail.com>
2021-09-06 17:04:26 +0300

    pyCpkg update

--- runner.py
+++ runner.py
@@ -1,10 +1,23 @@
+# import uvicorn
+#
+# from alexcode import app
+#
+# uvicorn.run(
+#     app,
+#     host="0.0.0.0",
+#     port=8000,
+#     log_level="debug",
+# )
+
+
 import uvicorn
 
 from alexcode import app
 
-uvicorn.run(
-    app,
-    host="0.0.0.0",
-    port=8000,
-    log_level="debug",
-)
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+        log_level="debug",
+    )
