97ba7879b56cdbfc40d0347b12f30897e962f716
EvgenijBekish <yauhenibekish@gmail.com>
2021-09-14 20:08:40 +0300

    Commit DEBUG

--- superproject/project/urls.py
+++ superproject/project/urls.py
@@ -8,9 +8,13 @@ from django.urls import path
 def hello_world(request: HttpRequest):
     return HttpResponse("hello world")
 
+def WTF(request: HttpRequest):
+    return HttpResponse("I did smth")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("hw/", hello_world),
     path("task4/", include("task4.urls")),
+    path("task5/", WTF),
 ]
